153ccf10b16e2c8c4215c72e857feeef312a9b04
Rohit Kumar <146456631+rohityadurudra@users.noreply.github.com>
2025-06-02 04:11:17 +0530

    Add files via upload

---
+++ sorting/bubblesort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    int swapped;
+
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0; // Optimization: track if any swapping is done
+
+        for (j = 0; j < n - i - 1; j++) {
+            // Swap if elements are in wrong order
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        // If no two elements were swapped in inner loop, array is sorted
+        if (swapped == 0)
+            break;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {5, 1, 4, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array:   ");
+    printArray(arr, n);
+
+    return 0;
+}
