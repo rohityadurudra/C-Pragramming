03437c870f31f2d7798d53ca3595d6102c75c1ef
Rohit Kumar <146456631+rohityadurudra@users.noreply.github.com>
2025-06-14 22:58:15 +0530

    Create BSTSearch.c

---
+++ BSTSearch.c
@@ -0,0 +1,8 @@
+Node* search(Node* root, int key) {
+    if (root == NULL || root->data == key)
+        return root;
+    if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
